860bda066e99c938bbd0c7dc1b2759158aec6dbc
Andre <andriearshavin2@gmail.com>
2026-04-12 10:37:47 +0700

    update ff duo

--- router/maker/fakeffduo.ts
+++ router/maker/fakeffduo.ts
@@ -20,7 +20,7 @@ export default async function fakeffduo(req: Request, res: Response) {
     })
   }
 
-  const githubBaseUrl = "https://raw.githubusercontent.com/Raavfy-24/Maker/refs/heads/main"
+  const githubBaseUrl = "https://raw.githubusercontent.com/Andre-Learn/Maker/refs/heads/main"
   const max = 50
 
   if (bg < 1 || bg > max) {
@@ -32,7 +32,7 @@ export default async function fakeffduo(req: Request, res: Response) {
 
   try {
 
-    const bgUrl = `${githubBaseUrl}/FAKE%20FF%20DUO/${bg}.png`
+    const bgUrl = `${githubBaseUrl}/FF%20DUO/${bg}.png`
     const bgBuffer = (await axios.get(bgUrl, { responseType: "arraybuffer" })).data
 
     const background = await Canvas.loadImage(bgBuffer)
